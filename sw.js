608bfa283c4f4930da0f6bfa8bb991668dee9b38
code01-66 <bnmcode01@gmail.com>
2025-09-14 18:32:54 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,42 @@
+
+self.addEventListener('push', function (event) {
+    const data = event.data.json();
+    const options = {
+        body: data.body,
+        icon: data.icon || '/icon-192x192.png',
+        badge: '/icon-192x192.png',
+        data: {
+            url: data.url || '/',
+        }
+    };
+    event.waitUntil(
+        self.registration.showNotification(data.title, options)
+    );
+});
+
+self.addEventListener('notificationclick', function (event) {
+    event.notification.close();
+    const urlToOpen = event.notification.data.url;
+
+    event.waitUntil(
+        clients.matchAll({
+            type: 'window',
+            includeUncontrolled: true
+        }).then((clientList) => {
+            if (clientList.length > 0) {
+                let client = clientList[0];
+                for (let i = 0; i < clientList.length; i++) {
+                    if (clientList[i].focused) {
+                        client = clientList[i];
+                    }
+                }
+                return client.focus().then(c => c.navigate(urlToOpen));
+            }
+            return clients.openWindow(urlToOpen);
+        })
+    );
+});
+
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
